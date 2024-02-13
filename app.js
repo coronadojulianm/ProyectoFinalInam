406bfbd06c306870644b6453623f70f825d782c3
coronadojulianm <juliancoronadom@gmail.com>
2024-02-13 09:05:58 -0500

    Add novedades requeriment

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
 import express from 'express';
 import usuarioRouter from './src/routers/usuarios.router.js';
+import bodyParser from "body-parser";
+
+
+import novedad from './src/routers/novedades.router.js';
+
+
+
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json());
+
+//configuracion
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//rutas
+app.use('/novedad',novedad);
 app.use(usuarioRouter);
 
 app.listen(PORT, () => {
